a5391c4e56c071444aff15786e670a5e85f5a223
Keijiro Takahashi <keijiro@gmail.com>
2019-11-11 22:46:44 +0900

    Add MidiDeviceAssign

---
+++ Assets/Script/MidiDeviceAssign.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+
+namespace Minis
+{
+    //
+    // MIDI device assigner class that pairs a PlayerInput with a MIDI device
+    //
+    [RequireComponent(typeof(PlayerInput))]
+    public sealed class MidiDeviceAssign : MonoBehaviour
+    {
+        [SerializeField] int _channel = -1;
+        [SerializeField] string _productName = null;
+
+        void OnDeviceChange(InputDevice device, InputDeviceChange change)
+        {
+            var midiDevice = device as Minis.MidiDevice;
+            if (midiDevice == null) return;
+
+            // Channel matching
+            if (_channel >= 0 && midiDevice.channel != _channel) return;
+
+            // Product name matching
+            if (!string.IsNullOrEmpty(_productName) &&
+                !device.description.product.Contains(_productName)) return;
+
+            // Do pairing.
+            InputUser.PerformPairingWithDevice(device, GetComponent<PlayerInput>().user);
+        }
+
+        void Start()
+        {
+            InputSystem.onDeviceChange += OnDeviceChange;
+        }
+
+        void OnDestroy()
+        {
+            InputSystem.onDeviceChange -= OnDeviceChange;
+        }
+    }
+}
