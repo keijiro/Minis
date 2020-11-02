001ce04c0da504d0917df5ee78f6ad3ba3a01e84
Keijiro Takahashi <keijiro@gmail.com>
2020-11-02 13:19:05 +0900

    Cosmetic changes

--- Packages/jp.keijiro.minis/Runtime/MidiDevice.cs
+++ Packages/jp.keijiro.minis/Runtime/MidiDevice.cs
@@ -5,147 +5,137 @@ using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.Layouts;
 using UnityEngine.InputSystem.LowLevel;
 
-namespace Minis
+namespace Minis {
+
+//
+// Custom input device class that processes input from a MIDI channel
+//
+[InputControlLayout(stateType = typeof(MidiDeviceState), displayName = "MIDI Device")]
+public sealed class MidiDevice : InputDevice
 {
+    #region Public accessors
+
+    // MIDI channel number
     //
-    // Custom input device class that processes input from a single MIDI channel
+    // The first channel returns 0.
+    public int channel { get; private set; }
+
+    // Get an input control object bound for a specific note.
+    public MidiNoteControl GetNote(int noteNumber)
+      => _notes[noteNumber];
+
+    // Get an input control object bound for a specific control element (CC).
+    public MidiValueControl GetControl(int controlNumber)
+      => _controls[controlNumber];
+
+    // Will-note-on event
     //
-    [InputControlLayout(
-        stateType = typeof(MidiDeviceState),
-        displayName = "MIDI Device"
-    )]
-    public sealed class MidiDevice : InputDevice
+    // The input system fires this event before processing a note-on message on
+    // this device instance. It gives a target note and an input velocity as
+    // event arguments. Note that the MidiNoteControl hasn't been updated at
+    // this point.
+    public event Action<MidiNoteControl, float> onWillNoteOn
     {
-        #region Public accessors
-
-        // MIDI channel number
-        //
-        // The first channel returns 0.
-        public int channel { get; private set; }
-
-        // Get an input control object bound for a specific note.
-        public MidiNoteControl GetNote(int noteNumber)
-        {
-            return _notes[noteNumber];
-        }
-
-        // Get an input control object bound for a specific control (CC).
-        public MidiValueControl GetControl(int controlNumber)
-        {
-            return _controls[controlNumber];
-        }
-
-        // Note-on event
-        //
-        // Fired right before a note-on message is processed by this device.
-        // A target note is given with the first argument, and a input velocity
-        // value is given with the second argument. Note that the
-        // MidiNoteControl hasn't been updated at this point.
-        public event Action<MidiNoteControl, float> onWillNoteOn
-        {
-            // The action list is allocated in a lazy initialization fashion
-            // with the add method.
-            add => (_willNoteOnActions = _willNoteOnActions ??
-                    new List<Action<MidiNoteControl, float>>()).Add(value);
-            remove => _willNoteOnActions.Remove(value);
-        }
-
-        // Note-off event
-        //
-        // Fired right before a note-off message is processed by this device.
-        // A target note is given with the first argument. Note that the
-        // MidiNoteControl hasn't been updated at this point.
-        public event Action<MidiNoteControl> onWillNoteOff
-        {
-            // The action list is allocated in a lazy initialization fashion
-            // with the add method.
-            add => (_willNoteOffActions = _willNoteOffActions ??
-                    new List<Action<MidiNoteControl>>()).Add(value);
-            remove => _willNoteOffActions.Remove(value);
-        }
-
-        #endregion
-
-        #region Internal objects
-
-        MidiNoteControl [] _notes;
-        MidiValueControl [] _controls;
-
-        List<Action<MidiNoteControl, float>> _willNoteOnActions;
-        List<Action<MidiNoteControl>> _willNoteOffActions;
-
-        #endregion
-
-        #region MIDI event receiver (invoked from MidiPort)
-
-        internal void ProcessNoteOn(byte note, byte velocity)
-        {
-            // Update state with a delta event.
-            InputSystem.QueueDeltaStateEvent(_notes[note], velocity);
-
-            // Invoke note-on events (only when it exists).
-            var fvelocity = velocity / 127.0f;
-            if (_willNoteOnActions != null)
-                foreach (var action in _willNoteOnActions)
-                    action(_notes[note], fvelocity);
-        }
-
-        internal void ProcessNoteOff(byte note)
-        {
-            // Update state with a delta event.
-            InputSystem.QueueDeltaStateEvent(_notes[note], (byte)0);
-
-            // Invoke note-on events (only when it exists).
-            if (_willNoteOffActions != null)
-                foreach (var action in _willNoteOffActions)
-                    action(_notes[note]);
-        }
-
-        internal void ProcessControlChange(byte number, byte value)
-        {
-            // Update state with a delta event.
-            InputSystem.QueueDeltaStateEvent(_controls[number], value);
-        }
-
-        #endregion
-
-        #region InputDevice implementation
-
-        protected override void FinishSetup()
-        {
-            base.FinishSetup();
-
-            // Populate the input controls.
-            _notes = new MidiNoteControl[128];
-            _controls = new MidiValueControl[128];
-
-            for (var i = 0; i < 128; i++)
-            {
-                _notes[i] = GetChildControl<MidiNoteControl>("note" + i.ToString("D3"));
-                _controls[i] = GetChildControl<MidiValueControl>("control" + i.ToString("D3"));
-            }
-
-            // Retrieve the MIDI channel number
-            // Here is a quite dirty trick: Parse the last two characters in
-            // the product name and use it as its channel number.
-            var product = description.product;
-            channel = int.Parse(product.Substring(product.Length - 2));
-        }
-
-        public static MidiDevice current { get; private set; }
-
-        public override void MakeCurrent()
-        {
-            base.MakeCurrent();
-            current = this;
-        }
-
-        protected override void OnRemoved()
-        {
-            base.OnRemoved();
-            if (current == this) current = null;
-        }
-
-        #endregion
+        // Action list lazy allocation
+        add => (_willNoteOnActions = _willNoteOnActions ??
+                new List<Action<MidiNoteControl, float>>()).Add(value);
+        remove => _willNoteOnActions.Remove(value);
     }
+
+    // Will-note-off event
+    //
+    // The input system fires this event before processing a note-off message
+    // on this device instance. It gives a target note as an event argument.
+    // Note that the MidiNoteControl hasn't been updated at this point.
+    public event Action<MidiNoteControl> onWillNoteOff
+    {
+        // Action list lazy allocation
+        add => (_willNoteOffActions = _willNoteOffActions ??
+                new List<Action<MidiNoteControl>>()).Add(value);
+        remove => _willNoteOffActions.Remove(value);
+    }
+
+    #endregion
+
+    #region Internal objects
+
+    MidiNoteControl [] _notes;
+    MidiValueControl [] _controls;
+
+    List<Action<MidiNoteControl, float>> _willNoteOnActions;
+    List<Action<MidiNoteControl>> _willNoteOffActions;
+
+    #endregion
+
+    #region MIDI event receiver (invoked from MidiPort)
+
+    internal void ProcessNoteOn(byte note, byte velocity)
+    {
+        // State update with a delta event
+        InputSystem.QueueDeltaStateEvent(_notes[note], velocity);
+
+        // Note-on event invocation (only when it exists)
+        var fvelocity = velocity / 127.0f;
+        if (_willNoteOnActions != null)
+            foreach (var action in _willNoteOnActions)
+                action(_notes[note], fvelocity);
+    }
+
+    internal void ProcessNoteOff(byte note)
+    {
+        // State update with a delta event
+        InputSystem.QueueDeltaStateEvent(_notes[note], (byte)0);
+
+        // Note-off event invocation (only when it exists)
+        if (_willNoteOffActions != null)
+            foreach (var action in _willNoteOffActions)
+                action(_notes[note]);
+    }
+
+    internal void ProcessControlChange(byte number, byte value)
+      // State update with a delta event
+      => InputSystem.QueueDeltaStateEvent(_controls[number], value);
+
+    #endregion
+
+    #region InputDevice implementation
+
+    protected override void FinishSetup()
+    {
+        base.FinishSetup();
+
+        // Populate the input controls.
+        _notes = new MidiNoteControl[128];
+        _controls = new MidiValueControl[128];
+
+        for (var i = 0; i < 128; i++)
+        {
+            _notes[i] = GetChildControl<MidiNoteControl>("note" + i.ToString("D3"));
+            _controls[i] = GetChildControl<MidiValueControl>("control" + i.ToString("D3"));
+        }
+
+        // MIDI channel number determination
+        // Here is a dirty trick: Parse the last two characters in the product
+        // name and use it as a channel number.
+        var product = description.product;
+        channel = int.Parse(product.Substring(product.Length - 2));
+    }
+
+    public static MidiDevice current { get; private set; }
+
+    public override void MakeCurrent()
+    {
+        base.MakeCurrent();
+        current = this;
+    }
+
+    protected override void OnRemoved()
+    {
+        base.OnRemoved();
+        if (current == this) current = null;
+    }
+
+    #endregion
 }
+
+} // namespace Minis
