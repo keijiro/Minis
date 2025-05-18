6b2491f5eaf162d74d05fab037d64333c47a7c38
Keijiro Takahashi <keijiro@gmail.com>
2025-05-18 18:50:19 +0900

    Add InputActionTest

---
+++ Assets/Script/InputActionTest.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using Unity.Mathematics;
+
+sealed public class InputActionTest : MonoBehaviour
+{
+    #region Input actions exposed to Inspector
+
+    [SerializeField] InputAction[] _noteActions = null;
+    [SerializeField] InputAction _modWheelAction = null;
+
+    #endregion
+
+    #region Private members
+
+    static string[] NoteNames = new[] { "C", "C#", "D", "D#", "E", "F",
+                                        "F#", "G", "G#", "A", "A#", "B" };
+    float[] _noteFades = new float[12];
+
+    #endregion
+
+    #region InputAction handlers
+
+    void SetUpNoteAction(int index)
+    {
+        var action = _noteActions[index];
+        action.performed += (ctx) => OnNotePerformed(ctx, index);
+        action.canceled += (ctx) => OnNoteCanceled(ctx, index);
+        action.Enable();
+    }
+
+    void OnNotePerformed(InputAction.CallbackContext ctx, int index)
+      => _noteFades[index] = 1;
+
+    void OnNoteCanceled(InputAction.CallbackContext ctx, int index)
+      => _noteFades[index] = 0;
+
+    #endregion
+
+    #region MonoBehaviour implementation
+
+    void Start()
+    {
+        for (var i = 0; i < 12; i++) SetUpNoteAction(i);
+        _modWheelAction.Enable();
+    }
+
+    void Update()
+    {
+        var decay = Time.deltaTime * 4;
+        var text = "This sample only accept notes from octave 3 to 5.\n\n";
+
+        for (var i = 0; i < 12; i++)
+        {
+            var fade = _noteFades[i];
+            var pressure = _noteActions[i].ReadValue<float>();
+            text += MakeRowText(NoteNames[i], pressure, fade);
+            _noteFades[i] = math.max(0, fade - decay);
+        }
+
+        var mod = _modWheelAction.ReadValue<float>();
+        text += MakeRowText("Mod Wheel", mod);
+
+        InfoLabelUI.text = text;
+    }
+
+    #endregion
+
+    #region UI helpers
+
+    Label InfoLabelUI
+      => GetComponent<UIDocument>().rootVisualElement.Q<Label>("info-label");
+
+    string MakeRowText(string name, float value, float fade = 1)
+    {
+        const int width = 30;
+        var bar = (int)(value * width);
+        var hex = $"{(int)(fade * 0x7f + 0x80):X2}";
+        var text = $"{name,10} |<color=#{hex}{hex}{hex}>";
+        text += new string('*', bar);
+        text += new string(' ', width - bar);
+        text += $"</color>| {value * 100,3:0}%\n";
+        return text;
+    }
+
+    #endregion
+}
